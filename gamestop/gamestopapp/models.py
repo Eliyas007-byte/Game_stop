d14e31084db61a6ee54c71da7552aef052edb215
Eliyas007-byte <eliyasmulla79@gmail.com>
2024-07-26 16:52:21 +0530

    message first

---
+++ gamestop/gamestopapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Product(models.Model):
+
+    type = (('Fighting','fighting'), ('Action','Action'), ('Shooter','Shooter'), ('Sports','Sports'), ('Battle Royale','Battle Royale'))
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=type)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to = 'image')
+
+
+
+
+class Cart(models.Model):
+
+    type = ((1,1), (2,2), (3,3), (4,4), (5,5))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    quantity = models.IntegerField(choices=type)
+    total_price = models.IntegerField()
+
+
+class orders (models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.IntegerField()
+
+
+
+class Review(models.Model):
+
+    type = ((1,1), (2,2), (3,3), (4,4), (5,5))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=type)
+    image = models.ImageField(upload_to='reviewimage')
+ 
